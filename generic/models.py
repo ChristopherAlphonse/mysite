db687ae13c21b2ea99e16e5a2a6aec152f183166
Christopher Alphonse <christopheralphonse96@gmail.com>
2022-10-02 21:38:04 -0700

    cc

--- generic/models.py
+++ generic/models.py
@@ -5,11 +5,23 @@ from wagtail.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 
 
+
 class GenericPage(Page):
    banner_title = models.CharField(
    max_length=255, 
    default="Welcome to Generic")
+   
+   introduction = models.TextField(blank=True)
+   banner_image = models.ForeignKey(
+      "wagtailimages.Image",
+      null=True,
+      blank=True,
+      on_delete=models.SET_NULL,
+      related_name="+"
+   )
 
    content_panels = Page.content_panels + [
-      FieldPanel('banner_title')
+      FieldPanel('banner_title'),
+      FieldPanel("introduction"),
+      FieldPanel("banner_image"),
 ]
